6dc5c6311f1f095b2bae34b4fec4d51167c3f1c9
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2023-07-04 15:21:15 +0200

    fix api

--- api/routes/api.route.js
+++ api/routes/api.route.js
@@ -3,6 +3,12 @@ const studentsRouter = require("./students.route");
 
 const apiRouter = express.Router();
 
+apiRouter.use((req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    next();
+});
+
 // Student routes
 apiRouter.use("/students", studentsRouter);
 
